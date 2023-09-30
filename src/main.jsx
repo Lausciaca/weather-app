74f02f9b34a031324ab9fee41327b29e3213a7fc
Laureano <laureanosciacaluga@gmail.com>
2023-09-30 07:27:04 -0300

    Otras instalaciones necesarias

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import Home from './views/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Home />
 )
