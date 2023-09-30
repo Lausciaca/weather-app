f81a40f64c97ac493734ac2556478a7537d2809f
Laureano <laureanosciacaluga@gmail.com>
2023-09-30 09:00:00 -0300

    V0.3.2 modificacion estilos del componente NextDayCard

--- src/components/NextDayCard.jsx
+++ src/components/NextDayCard.jsx
@@ -3,12 +3,12 @@ function NextDayCard ({day, }) {
     return (
         <article className=" w-1/5 h-4/5 flex flex-col items-start justify-evenly relative">
             <div className="absolute top-0 right-0">
-                <img src="src/assets/svg/soleado.svg" alt="Clima del dia" />
+                <img src="src/assets/svg/nublado/nublado.svg" alt="Clima del dia" className="h-36" />
             </div>
-            <div className="card w-5/6 h-60 text-center flex flex-col justify-end p-3 rounded-xl gap-5
+            <div className="card h-60 text-center flex flex-col justify-end p-3 rounded-xl gap-5
             ">
                 <div>
-                    <h2 className="day-text text-lg"><span> { day } </span>, 20-31°C</h2>
+                    <h2 className="day-text"><span className=" text-2xl"> { day } </span>, 20-31°C</h2>
                 </div>
                 <div>
                     <h2>Parcialmente nublado</h2>
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -7,11 +7,16 @@ function Home () {
             <section className="w-1/3 h-full">
                 <ThisDayCard />
             </section>
-            <section className="w-2/3 h-1/2 flex justify-around">
-                <NextDayCard day={'Miercoles'} />
-                <NextDayCard day={'Jueves'} />
-                <NextDayCard day={'Viernes'} />
-                <NextDayCard day={'Sabado'} />
+            <section className="w-2/3 h-1/2">
+                <section className="w-full h-full flex justify-around">
+                    <NextDayCard day={'Miercoles'} />
+                    <NextDayCard day={'Jueves'} />
+                    <NextDayCard day={'Viernes'} />
+                    <NextDayCard day={'Sabado'} />
+                </section>
+                <section>
+                    
+                </section>
             </section>
         </main>
     )
