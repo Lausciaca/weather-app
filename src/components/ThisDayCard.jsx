c619112acbeff613a994dad808aa8a96641ff5ce
Laureano <laureanosciacaluga@gmail.com>
2023-10-04 12:49:40 -0300

    V0.5.1 implementado api a la tarjeta del dia

--- src/components/ThisDayCard.jsx
+++ src/components/ThisDayCard.jsx
@@ -1,22 +1,55 @@
+import { useEffect } from "react"
+import { useState } from "react"
+
 function ThisDayCard () {
+    const [data, setData] = useState(null)
+    
+    const apiUrl = "http://api.weatherapi.com/v1/current.json?key=f6df8e4a1f9b409a80d140642230410&q=auto:ip&aqi=yes";
+
+    useEffect(() => {
+        fetch(apiUrl)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setData(data);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
 
     return (
          <article className="card w-8/12 h-4/5 flex flex-col items-center justify-evenly rounded-xl">
-            <div>
-                <p className="text-base">Rosario, Argentina</p>
-            </div>
-            <div>
-                <h2 className="day-text text-5xl"><span>Martes</span>, 31°C</h2>
-            </div>
-            <div>
-                <img src="src/assets/svg/soleado.svg" alt="Clima del dia" className="w-72" />
-            </div>
-            <div>
-                <h2 className="day-text text-2xl"><strong>Soleado</strong></h2>
-            </div>
-            <div>
-                <h3>Lluvia 10%</h3>
-            </div>
+            {data && (
+                <div>
+                    <p className="text-base">{data.location.name}, {data.location.country}</p>
+                </div>
+            )}
+            {data && (
+                <div>
+                    <h2 className="day-text text-5xl"><span>Martes</span>, {data.current.temp_c}°C</h2>
+                </div>
+            )}
+            {data && (
+                <div>
+                    <img src={`http:${data.current.condition.icon}`} alt="Clima del día" className="w-72" />
+                </div>
+            )}
+            {data && (
+                <div>
+                    <h2 className="day-text text-2xl"><strong>{data.current.condition.text}</strong></h2>
+                </div>
+            )}
+            {data && (
+                <div>
+                    <h3>Rain {data.current.precip_mm}%</h3>
+                </div>
+            )}
          </article>
     )
 }
