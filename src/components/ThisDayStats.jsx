644dd2a5f26602ec21aade81cfa6345ed33593a0
Laureano <laureanosciacaluga@gmail.com>
2023-10-04 09:32:40 -0300

    V0.4.2 implementado estilos al ThisDayStats

--- src/components/ThisDayStats.jsx
+++ src/components/ThisDayStats.jsx
@@ -7,23 +7,39 @@ function ThisDayStats() {
                 <article className="article-stats">
                     <h2 className="h-1/4 text-2xl p-2">Indice UV</h2>
                     <div className="h-3/4">
-                        <ProgressSemicircle color={'orange'} indice={7}/>
+                        <ProgressSemicircle color={'orange'} indice={10} indiceTexto={10} limite={16}/>
                     </div>
                 </article>
                 <article className="article-stats">
-
+                    <h2 className="h-1/4 text-2xl p-2">Viento</h2>
+                    <div className="h-3/4 flex flex-col justify-evenly">
+                        <p className="text-center text-5xl font-bold">Sur</p>
+                        <p className="text-center">22KM/H</p>
+                    </div>
                 </article>
                 <article className="article-stats">
-
+                    <h2 className="h-1/4 text-2xl p-2">AM-PM</h2>
+                    <div className="h-3/4">
+                        <ProgressSemicircle color={'orange'} indice={'10'} indiceTexto={'7:00-20:00'} limite={16}/>
+                    </div>
                 </article>
                 <article className="article-stats">
-
+                    <h2 className="h-1/4 text-2xl p-2">Humedad</h2>
+                    <div className="h-3/4 flex justify-center items-center">
+                        <p className="text-center text-5xl font-bold">43%</p>
+                    </div>
                 </article>
                 <article className="article-stats">
-
+                    <h2 className="h-1/4 text-2xl p-2">Sensacion termica</h2>
+                    <div className="h-3/4 flex justify-center items-center">
+                        <p className="text-center text-5xl font-bold">22°C</p>
+                    </div>
                 </article>
                 <article className="article-stats">
-
+                    <h2 className="h-1/4 text-2xl p-2">Calidad de aire</h2>
+                    <div className="h-3/4">
+                        <ProgressSemicircle color={'orange'} indice={25} indiceTexto={25} limite={160}/>
+                    </div>
                 </article>
             </div>
         </section>
--- src/components/progress-semicircle/ProgressSemicircle.jsx
+++ src/components/progress-semicircle/ProgressSemicircle.jsx
@@ -1,5 +1,5 @@
 import './ProgressSemicircle.css'
-function ProgressSemicircle ({indice, color}) {
+function ProgressSemicircle ({indice, indiceTexto, color, limite}) {
     
     return(
         <div className='w-full h-full relative flex items-end justify-center'>
@@ -7,9 +7,9 @@ function ProgressSemicircle ({indice, color}) {
             className='w-full h-full absolute'
             viewBox="0 0 150 100"
             >
-            <path d="M 0 100 A 60 60 0 0 1 150 100 Z" stroke={color} pathLength={'18'} strokeDasharray={indice + ' 200'}/>
+            <path d="M 0 100 A 60 60 0 0 1 150 100 Z" stroke={color} pathLength={limite} strokeDasharray={indice + ' 200'}/>
             </svg>
-            <p className='text-3xl'>{indice}</p>
+            <p className='text-3xl'>{indiceTexto}</p>
         </div>
         
 
